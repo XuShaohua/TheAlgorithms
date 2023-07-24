71c80e6ce0ca4d6b40376a875542c74ff4300154
Xu Shaohua <shaohua@biofan.org>
2023-07-24 10:05:45 +0800

    math: Add buf cache to fibonacci

--- math/src/fibonacci.rs
+++ math/src/fibonacci.rs
@@ -13,12 +13,12 @@ pub enum FibError {
     IllegalIndex,
 }
 
-pub const MAX_INDEX: i8 = 47;
+pub const MAX_INDEX: u8 = 47;
 
 /// # Errors
 /// Returns error if `num` is not a possitive integer.
-pub fn fib(num: i8) -> Result<i64, FibError> {
-    if num <= 0 || num > MAX_INDEX {
+pub fn fib(num: u8) -> Result<i64, FibError> {
+    if num == 0 || num > MAX_INDEX {
         return Err(FibError::IllegalIndex);
     }
     if num == 1 {
@@ -27,7 +27,15 @@ pub fn fib(num: i8) -> Result<i64, FibError> {
     if num == 2 {
         return Ok(1);
     }
-    Ok(fib(num - 1)? + fib(num - 2)?)
+    let num = num as usize;
+    let mut buf: Vec<i64> = vec![0; num + 1];
+    buf[0] = 0;
+    buf[1] = 0;
+    buf[2] = 1;
+    for i in 3..=num {
+        buf[i] = buf[i - 1] + buf[i - 2];
+    }
+    Ok(buf[num])
 }
 
 #[cfg(test)]
@@ -39,6 +47,7 @@ mod tests {
         assert_eq!(fib(5), Ok(3));
         assert_eq!(fib(2), Ok(1));
         assert_eq!(fib(9), Ok(21));
+        assert_eq!(fib(47), Ok(1836311903));
         assert_eq!(fib(49), Err(FibError::IllegalIndex));
     }
 }
