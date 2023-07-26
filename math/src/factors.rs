c9f1b50f6ee3acd43255d716f65d5fd467392853
Xu Shaohua <shaohua@biofan.org>
2023-07-26 08:24:48 +0800

    math: Add get_factor_list()

--- math/src/factors.rs
+++ math/src/factors.rs
@@ -39,9 +39,42 @@ pub fn get_factors(mut n: u64) -> Counter {
     factors
 }
 
+/// Returns all available factors of a possitive integer.
+#[must_use]
+#[allow(clippy::cast_possible_truncation)]
+#[allow(clippy::cast_precision_loss)]
+#[allow(clippy::cast_sign_loss)]
+pub fn get_factor_list(num: u64) -> Vec<u64> {
+    let mut factors = Vec::new();
+    if num < 1 {
+        return factors;
+    }
+    factors.push(1);
+    if num == 1 {
+        return factors;
+    }
+
+    factors.push(num);
+    for i in 2..=((num as f64).sqrt() as u64) {
+        // If i is a factor of num
+        if num % i == 0 {
+            factors.push(i);
+
+            // num//i is the other factor of num
+            let d: u64 = num / i;
+            // If d and i are distinct
+            if d != i {
+                factors.push(d);
+            }
+        }
+    }
+    factors.sort_unstable();
+    factors
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{get_factors, Counter};
+    use super::{get_factor_list, get_factors, Counter};
 
     #[test]
     fn test_get_factors() {
@@ -50,5 +83,12 @@ mod tests {
             get_factors(2520),
             Counter::from([(2, 3), (3, 2), (5, 1), (7, 1)])
         );
+    }
+
+    #[test]
+    fn test_get_factor_list() {
+        assert_eq!(get_factor_list(1), vec![1]);
+        assert_eq!(get_factor_list(5), vec![1, 5]);
+        assert_eq!(get_factor_list(24), vec![1, 2, 3, 4, 6, 8, 12, 24]);
     }
 }
