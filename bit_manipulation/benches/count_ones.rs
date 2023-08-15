034e0732d5d81284dd552e0ec8316c6c627b983e
Xu Shaohua <shaohua@biofan.org>
2023-08-15 09:25:30 +0800

    bit: Add bench to trailing-zeros

--- bit_manipulation/benches/count_ones.rs
+++ bit_manipulation/benches/count_ones.rs
@@ -8,33 +8,39 @@ use bit_manipulation::count_number_of_one_bits::{
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn brian_bench(c: &mut Criterion) {
-    c.bench_function("bit::brian-25", |b| {
+    c.bench_function("bit::count_ones-brian-25", |b| {
         b.iter(|| brian_kernighans_algorithm(black_box(25)))
     });
-    c.bench_function("bit::brian-37", |b| {
+    c.bench_function("bit::count_ones-brian-37", |b| {
         b.iter(|| brian_kernighans_algorithm(black_box(37)))
     });
-    c.bench_function("bit::brian-58", |b| {
+    c.bench_function("bit::count_ones-brian-58", |b| {
         b.iter(|| brian_kernighans_algorithm(black_box(58)))
     });
 }
 
 pub fn modulo_bench(c: &mut Criterion) {
-    c.bench_function("bit::modulo-25", |b| {
+    c.bench_function("bit::count_ones-modulo-25", |b| {
         b.iter(|| modulo_operator(black_box(25)))
     });
-    c.bench_function("bit::modulo-37", |b| {
+    c.bench_function("bit::count_ones-modulo-37", |b| {
         b.iter(|| modulo_operator(black_box(37)))
     });
-    c.bench_function("bit::modulo-58", |b| {
+    c.bench_function("bit::count_ones-modulo-58", |b| {
         b.iter(|| modulo_operator(black_box(58)))
     });
 }
 
 pub fn builtin_bench(c: &mut Criterion) {
-    c.bench_function("bit::builtin-25", |b| b.iter(|| builtin(black_box(25))));
-    c.bench_function("bit::builtin-37", |b| b.iter(|| builtin(black_box(37))));
-    c.bench_function("bit::builtin-58", |b| b.iter(|| builtin(black_box(58))));
+    c.bench_function("bit::count_ones-builtin-25", |b| {
+        b.iter(|| builtin(black_box(25)))
+    });
+    c.bench_function("bit::count_ones-builtin-37", |b| {
+        b.iter(|| builtin(black_box(37)))
+    });
+    c.bench_function("bit::count_ones-builtin-58", |b| {
+        b.iter(|| builtin(black_box(58)))
+    });
 }
 
 criterion_group!(benches, brian_bench, modulo_bench, builtin_bench,);
---
+++ bit_manipulation/benches/trailing_zeros.rs
@@ -0,0 +1,33 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use bit_manipulation::binary_count_trailing_zeros::{builtin, count_trailing_zeros};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn brute_force_bench(c: &mut Criterion) {
+    c.bench_function("bit::trailing_zero-brute-25", |b| {
+        b.iter(|| count_trailing_zeros(black_box(25)))
+    });
+    c.bench_function("bit::trailing_zero-brute-37", |b| {
+        b.iter(|| count_trailing_zeros(black_box(37)))
+    });
+    c.bench_function("bit::trailing-zero-brute-58", |b| {
+        b.iter(|| count_trailing_zeros(black_box(58)))
+    });
+}
+
+pub fn builtin_bench(c: &mut Criterion) {
+    c.bench_function("bit::trailing_zero-builtin-25", |b| {
+        b.iter(|| builtin(black_box(25)))
+    });
+    c.bench_function("bit::trailing_zero-builtin-37", |b| {
+        b.iter(|| builtin(black_box(37)))
+    });
+    c.bench_function("bit::trailing_zero-builtin-58", |b| {
+        b.iter(|| builtin(black_box(58)))
+    });
+}
+
+criterion_group!(benches, brute_force_bench, builtin_bench,);
+criterion_main!(benches);
