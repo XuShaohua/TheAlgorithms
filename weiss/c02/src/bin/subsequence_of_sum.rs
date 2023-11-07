914061b7b6af2b1ac80dbfc4cbd233ff4ac18cab
Xu Shaohua <shaohua@biofan.org>
2023-11-07 20:29:14 +0800

    weiss: Add subseq method1

--- weiss/c02/src/bin/subsequence_of_sum.rs
+++ weiss/c02/src/bin/subsequence_of_sum.rs
@@ -11,4 +11,34 @@
 //!
 //! For input -2, 11, -4, 13, -5, -2, the answer is 20 (a 2 through a4).
 
-fn main() {}
+fn method1(nums: &[i32]) -> i32 {
+    let mut max_sum = 0;
+    let len = nums.len();
+    for i in 0..len {
+        for j in i..len {
+            let this_sum = nums[i..=j].iter().sum();
+            max_sum = max_sum.max(this_sum);
+        }
+    }
+
+    max_sum
+}
+
+fn main() {
+    const NUMS: &[i32] = &[4, -3, 5, -2, -1, 2, 6, -2];
+    let r = method1(NUMS);
+    println!("best sum: {r}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::method1;
+
+    const NUMS: &[i32] = &[4, -3, 5, -2, -1, 2, 6, -2];
+
+    #[test]
+    fn test_method1() {
+        let r = method1(NUMS);
+        assert_eq!(r, 11);
+    }
+}
