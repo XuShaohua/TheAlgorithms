783d023d9c8d8c2e814947d970104b971a63baaf
Xu Shaohua <shaohua@biofan.org>
2023-11-16 14:52:43 +0800

    robert: Remove Generics

--- robert/src/util.rs
+++ robert/src/util.rs
@@ -5,15 +5,6 @@
 use std::fmt;
 use std::io::{self, BufRead, BufReader};
 
-pub fn exch<T>(list: &mut Vec<T>, i: usize, j: usize)
-where
-    T: Clone,
-{
-    let tmp = list[i].clone();
-    list[i] = list[j].clone();
-    list[j] = tmp.clone();
-}
-
 pub fn is_sorted<T>(list: &[T]) -> bool
 where
     T: PartialOrd + fmt::Debug,
@@ -31,36 +22,14 @@ where
     return true;
 }
 
-pub fn show<T>(vec: &[T])
+pub fn show<T>(slice: &[T])
 where
     T: fmt::Display,
 {
-    for s in vec {
+    for s in slice {
         print!("{} ", s);
     }
     println!();
-}
-
-#[allow(dead_code)]
-struct Generics<T> {
-    t: std::marker::PhantomData<T>,
-}
-
-impl<T: Copy + std::fmt::Display> Generics<T> {
-    #[allow(dead_code)]
-    pub fn exch(list: &mut Vec<T>, i: usize, j: usize) {
-        let tmp = list[i];
-        list[i] = list[j];
-        list[j] = tmp;
-    }
-
-    #[allow(dead_code)]
-    pub fn show(vec: &Vec<T>) {
-        for s in vec {
-            print!("{} ", s);
-        }
-        println!("");
-    }
 }
 
 /// Read integers from stdin.
