02aafb84b6e542e8358831522f2d95e52968b7c9
Xu Shaohua <shaohua@biofan.org>
2023-11-29 23:22:27 +0800

    cii: Add arith interface

--- cii/src/arith.c
+++ cii/src/arith.c
@@ -2,9 +2,4 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
-#include "cii/set.h"
-
-#include <limits.h>
-
-#include "cii/assert.h"
-#include "cii/mem.h"
+#include "cii/arith.h"
