302b832c14b630cefae6c90ae96c372678e099f0
Xu Shaohua <shaohua@biofan.org>
2023-12-03 23:02:39 +0800

    c: Remove sleep

--- c/thread/three_threads.cpp
+++ c/thread/three_threads.cpp
@@ -2,7 +2,6 @@
 // Use of this source is governed by General Public License that can be
 // found in the LICENSE file.
 
-#include <chrono>
 #include <iostream>
 #include <mutex>
 #include <thread>
@@ -19,27 +18,22 @@ void print_nums(int tid) {
     if (g_num_count < 100) {
       g_num_count += 1;
       is_running = true;
+      std::cout << "tid: " << tid << " +" << (local_num_count + 1)<< std::endl;
     }
   }
 
   if (is_running) {
-    std::cout << "tid: " << tid << " +" << (local_num_count + 1)<< std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
     print_nums(tid);
   }
 }
 
-void method1() {
+int main(void) {
   std::thread t1(print_nums, 1);
   std::thread t2(print_nums, 2);
   std::thread t3(print_nums, 3);
   t1.join();
   t2.join();
   t3.join();
-}
-
-int main(void) {
-  method1();
 
   return 0;
 }
