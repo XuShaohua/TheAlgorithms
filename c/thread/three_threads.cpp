e6125b22606b69e295561f42c211a45768a01692
Xu Shaohua <shaohua@biofan.org>
2023-12-03 23:07:39 +0800

    thread: Add number order

--- c/thread/three_threads.cpp
+++ c/thread/three_threads.cpp
@@ -10,21 +10,23 @@ std::mutex g_test_mutex;
 int g_num_count = 0;
 constexpr int kTargetNum = 100;
 
-void print_nums(int tid) {
+void print_nums(int tid, int num_order) {
   while (true) {
     std::lock_guard<std::mutex> lock_guard(g_test_mutex);
     if (g_num_count >= kTargetNum) {
       break;
     }
-    std::cout << "tid: " << tid << " +" << (g_num_count + 1)<< std::endl;
-    g_num_count += 1;
+    if (g_num_count % 3 == num_order) {
+      std::cout << "tid: " << tid << " +" << (g_num_count + 1)<< std::endl;
+      g_num_count += 1;
+    }
   }
 }
 
 int main(void) {
-  std::thread t1(print_nums, 1);
-  std::thread t2(print_nums, 2);
-  std::thread t3(print_nums, 3);
+  std::thread t1(print_nums, 1, 0);
+  std::thread t2(print_nums, 2, 1);
+  std::thread t3(print_nums, 3, 2);
   t1.join();
   t2.join();
   t3.join();
