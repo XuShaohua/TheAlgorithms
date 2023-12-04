e01644d11480ce5fbd99984d311ed3a4d3daf159
Xu Shaohua <shaohua@biofan.org>
2023-12-04 20:57:35 +0800

    leetcode: Fix atomic operation error

--- leetcode/1114.print-in-order/main.cpp
+++ leetcode/1114.print-in-order/main.cpp
@@ -21,27 +21,23 @@ class Foo {
  public:
   Foo() : counter_(0) {}
 
-  void first() { 
-    while (counter_.load(std::memory_order_seq_cst) != 0) {
-      // empty
-    }
-    counter_.store(1, std::memory_order_seq_cst);
+  void first(std::function<void()> printFirst) { 
     printFirst();
+    counter_.store(1, std::memory_order_seq_cst);
   }
 
-  void second() {
+  void second(std::function<void()> printSecond) {
     while (counter_.load(std::memory_order_seq_cst) != 1) {
       // empty
     }
-    counter_.store(2, std::memory_order_seq_cst);
     printSecond();
+    counter_.store(2, std::memory_order_seq_cst);
   }
 
-  void third() { 
+  void third(std::function<void()> printThird) { 
     while (counter_.load(std::memory_order_seq_cst) != 2) {
       // empty
     }
-    counter_.store(3, std::memory_order_seq_cst);
     printThird();
   }
 
@@ -51,9 +47,15 @@ class Foo {
 
 int main() {
   Foo foo;
-  std::thread a(&Foo::first, &foo);
-  std::thread b(&Foo::second, &foo);
-  std::thread c(&Foo::third, &foo);
+  std::thread a([&foo]() {
+      foo.first(printFirst);
+    });
+  std::thread b([&foo]() {
+      foo.second(printSecond);
+    });
+  std::thread c([&foo]() {
+      foo.third(printThird);
+    });
   a.join();
   b.join();
   c.join();
