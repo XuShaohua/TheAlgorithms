d05b18c78ae3f9cd4a302aa0564e133d45eb8368
Xu Shaohua <shaohua@biofan.org>
2024-03-20 17:43:35 +0800

    rust: Solve 0015 3sum

--- rust/0015.3sum/src/main.rs
+++ rust/0015.3sum/src/main.rs
@@ -1,26 +1,50 @@
-// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by General Public License that can be
 // found in the LICENSE file.
 
+/// Solve with two-pointers method.
 pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut nums = nums;
+    nums.sort();
     let mut result = Vec::new();
 
     let len = nums.len();
+
     for i in 0..(len - 2) {
-        for j in (i + 1)..(len - 1) {
-            for k in j..len {
-                if nums[i] + nums[j] + nums[k] == 0 {
-                    let mut ok_nums = vec![nums[i], nums[j], nums[k]];
-                    ok_nums.sort();
-                    if !result.contains(&ok_nums) {
-                        result.push(ok_nums);
-                    }
+        let target = -nums[i];
+
+        if target < 0 {
+            break;
+        }
+        if i > 0 && nums[i] == nums[i - 1] {
+            continue;
+        }
+
+        let mut left = i + 1;
+        let mut right = len - 1;
+
+        while left < right {
+            let sum = nums[left] + nums[right];
+            if sum < target {
+                left += 1;
+            } else if sum > target {
+                right -= 1;
+            } else {
+                result.push(vec![nums[i], nums[left], nums[right]]);
+                // Remove duplicates
+                while left < right && nums[left] == nums[left + 1] {
+                    left += 1;
                 }
+                while left < right && nums[right] == nums[right - 1] {
+                    right -= 1;
+                }
+
+                left += 1;
+                right -= 1;
             }
         }
     }
 
-    result.sort();
     result
 }
 
