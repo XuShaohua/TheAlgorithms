2160fc7dacdc6020cc2dd558b2a5c7bcef764e49
Xu Shaohua <shaohua@biofan.org>
2024-03-21 12:31:41 +0800

    cpp: Add template

---
+++ cpp/template/main.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be
+// found in the LICENSE file.
+
+#include <cassert>
+#include <cstdio>
+
+class Solution {
+ public:
+};
+
+
+void checkSolution() {
+}
+
+int main() {
+  checkSolution();
+  return 0;
+}
