92e78a96a8fbd470ba9d00da58f41ebf20f30b9d
Xu Shaohua <shaohua@biofan.org>
2024-03-21 15:40:57 +0800

    rust: Add method 2 for 0628

--- rust/0628.maximum-product-of-three-numbers/src/main.rs
+++ rust/0628.maximum-product-of-three-numbers/src/main.rs
@@ -2,6 +2,7 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
+// Sort array and filter based on rules.
 pub fn maximum_product(nums: Vec<i32>) -> i32 {
     let mut nums = nums;
     let len = nums.len();
@@ -20,9 +21,39 @@ pub fn maximum_product(nums: Vec<i32>) -> i32 {
     max_vals[max_vals.len() - 1]
 }
 
+/// Find 3 maximum and 2 minimum directly, no sorting.
+pub fn maximum_product2(nums: Vec<i32>) -> i32 {
+    let mut max1 = i32::MIN;
+    let mut max2 = i32::MIN;
+    let mut max3 = i32::MIN;
+    let mut min1 = i32::MAX;
+    let mut min2 = i32::MAX;
+    for num in nums {
+        if num > max1 {
+            max3 = max2;
+            max2 = max1;
+            max1 = num;
+        } else if num > max2 {
+            max3 = max2;
+            max2 = num;
+        } else if num > max3 {
+            max3 = num;
+        }
+
+        if num < min1 {
+            min2 = min1;
+            min1 = num;
+        } else if num < min2 {
+            min2 = num;
+        }
+    }
+
+    (min1 * min2 * max1).max(max1 * max2 * max3)
+}
+
 fn check_solution() {
     let nums = vec![-100, -98, -1, 2, 3, 4];
-    assert_eq!(maximum_product(nums), 39200);
+    assert_eq!(maximum_product2(nums), 39200);
 }
 
 fn main() {
