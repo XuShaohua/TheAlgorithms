da17eab31acd64aa7ae20502e2fbb43ee034e67f
Xu Shaohua <shaohua@biofan.org>
2024-03-21 13:35:01 +0800

    rust: Add method 2 to  0414

--- rust/0414.third-maximum-number/src/main.rs
+++ rust/0414.third-maximum-number/src/main.rs
@@ -2,6 +2,9 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
+pub type SolutionFunc = fn(Vec<i32>) -> i32;
+
+/// Sort vector and remove duplicates.
 pub fn third_max(nums: Vec<i32>) -> i32 {
     let mut nums = nums;
     nums.sort();
@@ -13,20 +16,52 @@ pub fn third_max(nums: Vec<i32>) -> i32 {
     }
 }
 
-fn check_solution() {
+/// Create an array to hold three maximum values.
+pub fn third_max2(nums: Vec<i32>) -> i32 {
+    let mut max_nums: [Option<i32>; 3] = [None; 3];
+    for num in nums {
+        let mut idx = None;
+        for (i, m) in max_nums.iter().enumerate() {
+            if m.is_some() && num == m.unwrap() {
+                break;
+            }
+
+            if m.is_none() || m.is_some() && num > m.unwrap() {
+                idx = Some(i);
+                break;
+            }
+        }
+
+        if let Some(idx) = idx {
+            for i in (idx + 1..3).rev() {
+                max_nums.swap(i, i - 1);
+            }
+            max_nums[idx] = Some(num);
+        }
+    }
+
+    if let Some(num) = max_nums[2] {
+        num
+    } else {
+        max_nums[0].unwrap()
+    }
+}
+
+fn check_solution(func: SolutionFunc) {
     let nums = vec![3, 2, 1];
-    assert_eq!(third_max(nums), 1);
+    assert_eq!(func(nums), 1);
 
     let nums = vec![1, 2];
-    assert_eq!(third_max(nums), 2);
+    assert_eq!(func(nums), 2);
 
     let nums = vec![2, 2, 3, 1];
-    assert_eq!(third_max(nums), 1);
+    assert_eq!(func(nums), 1);
 
     let nums = vec![1, 1, 2];
-    assert_eq!(third_max(nums), 2);
+    assert_eq!(func(nums), 2);
 }
 
 fn main() {
-    check_solution();
+    check_solution(third_max);
+    check_solution(third_max2);
 }
