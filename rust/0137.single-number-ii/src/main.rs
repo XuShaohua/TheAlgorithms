56aa0ea27da599738e023b1cbc21fd488132295d
Xu Shaohua <shaohua@biofan.org>
2024-03-25 22:47:17 +0800

    rust: Fix 0137

--- rust/0137.single-number-ii/src/main.rs
+++ rust/0137.single-number-ii/src/main.rs
@@ -2,36 +2,16 @@
 // Use of this source is governed by General Public License that can be
 // found in the LICENSE file.
 
-const DIGIT_LEN: usize = 32;
-pub type Digits = [i32; DIGIT_LEN];
-
-fn to_digits(mut num: i32) -> Digits {
-    let mut digits = [0; DIGIT_LEN];
-    for digit in digits.iter_mut() {
-        *digit = num % 2;
-        num /= 2;
-    }
-    digits
-}
-
-fn from_digits(digits: Digits) -> i32 {
-    let mut num = 0;
-    for i in (0..DIGIT_LEN).rev() {
-        num = num * 2 + digits[i];
-    }
-    num
-}
-
 // bit vector
 pub fn single_number(nums: Vec<i32>) -> i32 {
-    let mut all_digits: Digits = [0; DIGIT_LEN];
-    for num in &nums {
-        let num_digits = to_digits(*num);
-        for i in 0..DIGIT_LEN {
-            all_digits[i] = (all_digits[i] + num_digits[i]) % 3;
-        }
+    const DIGIT_LEN: usize = 32;
+
+    let mut ans = 0;
+    for i in 0..DIGIT_LEN {
+        let sum = nums.iter().map(|num| num >> i & 1).sum::<i32>();
+        ans |= (sum % 3) << i;
     }
-    from_digits(all_digits)
+    ans
 }
 
 fn check_solution() {
