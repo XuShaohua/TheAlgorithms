c8bd90200950254ea5abdcfce1275142d95ddadd
Xu Shaohua <shaohua@biofan.org>
2024-03-25 22:08:27 +0800

    rust: Add method 2 to 0268

--- rust/0268.missing-number/src/main.rs
+++ rust/0268.missing-number/src/main.rs
@@ -2,24 +2,32 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
+pub type SolutionFunc = fn(Vec<i32>) -> i32;
+
 pub fn missing_number(nums: Vec<i32>) -> i32 {
     let mut current = 0;
     for (i, num) in nums.iter().enumerate() {
         let i = i as i32;
         current = current ^ num ^ i;
     }
-    current = current ^ (nums.len() as i32);
+    current ^= nums.len() as i32;
     current
 }
 
-fn check_solution() {
+pub fn missing_number2(nums: Vec<i32>) -> i32 {
+    let n = nums.len() as i32;
+    (n * (n + 1)) / 2 - nums.iter().sum::<i32>()
+}
+
+fn check_solution(func: SolutionFunc) {
     let nums = vec![3, 0, 1];
-    assert_eq!(missing_number(nums), 2);
+    assert_eq!(func(nums), 2);
 
     let nums = vec![9, 6, 4, 2, 3, 5, 7, 0, 1];
-    assert_eq!(missing_number(nums), 8);
+    assert_eq!(func(nums), 8);
 }
 
 fn main() {
-    check_solution();
+    check_solution(missing_number);
+    check_solution(missing_number2);
 }
