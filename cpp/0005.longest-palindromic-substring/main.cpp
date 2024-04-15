e8936933b2b366eb1569105523be317268aaa39f
Xu Shaohua <shaohua@biofan.org>
2024-04-15 11:25:17 +0800

    cpp: Add 0485

--- cpp/0005.longest-palindromic-substring/main.cpp
+++ cpp/0005.longest-palindromic-substring/main.cpp
@@ -2,9 +2,17 @@
 // Use of this source is governed by General Public License that can be
 // found in the LICENSE file.
 
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+
+#include <string>
+
+class Solution {
+ public:
+  static std::string longestPalindrome(std::string s) {
+  }
+};
 
 int main() {
   return 0;
