58b536121f45c2c46dfcd8f4a1c516c4eb3af242
Xu Shaohua <shaohua@biofan.org>
2024-04-25 16:22:56 +0800

    0219: Add hash map method

--- rust/0219.contains-duplicate-ii/src/main.rs
+++ rust/0219.contains-duplicate-ii/src/main.rs
@@ -2,7 +2,10 @@
 // Use of this source is governed by General Public License that can be
 // found in the LICENSE file.
 
-pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+use std::collections::HashMap;
+
+// Brute Force
+pub fn contains_nearby_duplicate1(nums: Vec<i32>, k: i32) -> bool {
     let len = nums.len();
     let k = k as usize;
 
@@ -16,16 +19,53 @@ pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
     false
 }
 
-fn main() {
+// 使用 HashMap
+pub fn contains_nearby_duplicate2(nums: Vec<i32>, k: i32) -> bool {
+    let k = k as usize;
+
+    // map 用于存储数值及其在数组中的位置
+    let mut map = HashMap::<i32, usize>::with_capacity(nums.len());
+
+    // 遍历数组中所有元素
+    for (index, &num) in nums.iter().enumerate() {
+        // 在 map 中尝试查找这个元素, 并判断与当前遍历的元素是否重复.
+        if let Some(&old_index) = map.get(&num) {
+            if (index - old_index) <= k {
+                return true;
+            }
+        }
+        map.insert(num, index);
+    }
+    false
+}
+
+pub type SolutionFn = fn(Vec<i32>, i32) -> bool;
+
+fn check_solution(func: SolutionFn) {
     let nums = vec![1, 2, 3, 1];
     let k = 3;
-    assert!(contains_nearby_duplicate(nums, k));
+    assert!(func(nums, k));
 
     let nums = vec![1, 0, 1, 1];
     let k = 1;
-    assert!(contains_nearby_duplicate(nums, k));
+    assert!(func(nums, k));
 
     let nums = vec![1, 2, 3, 1, 2, 3];
     let k = 2;
-    assert!(!contains_nearby_duplicate(nums, k));
+    assert!(!func(nums, k));
+}
+
+fn main() {
+    check_solution(contains_nearby_duplicate1);
+    check_solution(contains_nearby_duplicate2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{check_solution, contains_nearby_duplicate1};
+
+    #[test]
+    fn test_contains_nearby_duplicate1() {
+        check_solution(contains_nearby_duplicate1);
+    }
 }
