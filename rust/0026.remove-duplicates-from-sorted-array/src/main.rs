38f52fff313def157dc19a0cbe8709978109a41b
Xu Shaohua <shaohua@biofan.org>
2024-04-27 05:53:24 +0800

    0026: Add two pointers and dedup()

--- rust/0026.remove-duplicates-from-sorted-array/src/main.rs
+++ rust/0026.remove-duplicates-from-sorted-array/src/main.rs
@@ -2,51 +2,60 @@
 // Use of this source is governed by General Public License that can be
 // found in the LICENSE file.
 
-pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+// 双指针 Two pointers
+#[allow(clippy::ptr_arg)]
+pub fn remove_duplicates1(nums: &mut Vec<i32>) -> i32 {
     assert!(!nums.is_empty());
-
-    let len = nums.len();
-    if len == 1 {
-        return 1;
-    }
-
-    let first_elem = nums[0];
-
-    let mut i = 0;
-    while i < len - 1 {
-        let mut duplicates = 0;
-        for j in (i + 1)..len {
-            if nums[i] == nums[j] {
-                nums[j] = first_elem;
-                duplicates += 1;
-            } else {
-                break;
-            }
-        }
-        i += duplicates + 1;
-    }
-
-    i = 1;
-    while i < nums.len() {
-        if nums[i] == first_elem {
-            nums.remove(i);
-        } else {
-            i += 1;
+    // 第一个指针, 用于记录当前不重复的位置
+    let mut idx = 1;
+    // 第二个指针, 用于遍历数组
+    for i in 1..nums.len() {
+        if nums[i - 1] != nums[i] {
+            nums[idx] = nums[i];
+            idx += 1;
         }
     }
+    idx as i32
+}
+
+// Vec 的去重函数, 支持已排序的
+pub fn remove_duplicates2(nums: &mut Vec<i32>) -> i32 {
+    nums.dedup();
     nums.len() as i32
 }
 
-fn main() {
+pub type SolutionFn = fn(&mut Vec<i32>) -> i32;
+
+fn check_solution(func: SolutionFn) {
     let mut nums = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
     let expected_nums = &[0, 1, 2, 3, 4];
-    let k = remove_duplicates(&mut nums);
-    assert_eq!(&nums, expected_nums);
+    let k = func(&mut nums);
     assert_eq!(k, 5);
+    assert_eq!(&nums[0..(k as usize)], expected_nums);
 
     let mut nums = vec![1, 1, 2];
     let expected_nums = &[1, 2];
-    let k = remove_duplicates(&mut nums);
-    assert_eq!(&nums, expected_nums);
+    let k = func(&mut nums);
     assert_eq!(k, 2);
+    assert_eq!(&nums[0..(k as usize)], expected_nums);
+}
+
+fn main() {
+    check_solution(remove_duplicates1);
+    check_solution(remove_duplicates2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{check_solution, remove_duplicates1, remove_duplicates2};
+
+    #[test]
+    fn test_remove_duplicates1() {
+        check_solution(remove_duplicates1);
+    }
+
+    #[test]
+    fn test_remove_duplicates2() {
+        check_solution(remove_duplicates2);
+    }
 }
