4a08e37d539bc96613f71e45e0da5bb0bf49cf7b
Xu Shaohua <shaohua@biofan.org>
2024-04-28 17:32:59 +0800

    0350: Add index file and flow charts

--- src/leetcode/0350.intersection-of-two-arrays-ii/src/main.rs
+++ src/leetcode/0350.intersection-of-two-arrays-ii/src/main.rs
@@ -47,7 +47,7 @@ pub fn intersect2(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
         (nums2, nums1)
     };
 
-    // 用哈稀表来给 nums1 计数.
+    // 用哈稀表来给 nums1 中的元素计数.
     let mut map1 = HashMap::new();
     for num in &nums1 {
         map1.entry(num).and_modify(|count| *count += 1).or_insert(1);
@@ -58,6 +58,7 @@ pub fn intersect2(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
     // 遍历 nums2.
     for num in &nums2 {
         if let Some(count) = map1.get_mut(num) {
+            // 如果该元素在 map1 中存在, 就将其计数值减1.
             *count -= 1;
             out.push(*num);
             if *count == 0 {
