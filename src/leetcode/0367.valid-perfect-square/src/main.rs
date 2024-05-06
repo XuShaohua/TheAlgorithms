59fc30722962f2d7591f338706f46b1289d452e2
Xu Shaohua <shaohua@biofan.org>
2024-05-06 17:53:22 +0800

    leetcode: Add 0050

--- src/leetcode/0367.valid-perfect-square/src/main.rs
+++ src/leetcode/0367.valid-perfect-square/src/main.rs
@@ -2,12 +2,16 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
-pub fn is_perfect_square(num: i32) -> bool {
-    use std::cmp::Ordering;
+use std::cmp::Ordering;
+
+// Binary Search
+pub fn is_perfect_square1(num: i32) -> bool {
+    assert!(num >= 1);
 
     let mut left = 1;
     let mut right = num;
     let num64 = num as i64;
+
     while left <= right {
         let middle = left + (right - left) / 2;
         let square = middle as i64 * middle as i64;
@@ -20,16 +24,27 @@ pub fn is_perfect_square(num: i32) -> bool {
     false
 }
 
-fn check_solution() {
-    assert!(is_perfect_square(1));
-    assert!(!is_perfect_square(8));
-    assert!(is_perfect_square(9));
-    assert!(!is_perfect_square(10));
-    assert!(!is_perfect_square(14));
-    assert!(is_perfect_square(16));
-    assert!(!is_perfect_square(i32::MAX));
+pub type SolutionFn = fn(i32) -> bool;
+
+fn check_solution(func: SolutionFn) {
+    assert!(func(1));
+    assert!(!func(8));
+    assert!(func(9));
+    assert!(!func(10));
+    assert!(func(16));
+    assert!(!func(i32::MAX));
 }
 
 fn main() {
-    check_solution();
+    check_solution(is_perfect_square1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{check_solution, is_perfect_square1};
+
+    #[test]
+    fn test_is_perfect_square1() {
+        check_solution(is_perfect_square1);
+    }
 }
