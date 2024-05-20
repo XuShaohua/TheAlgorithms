2a8d8ffd7e70e8922df82734ebab23a65978bdbf
Xu Shaohua <shaohua@biofan.org>
2024-05-20 18:51:12 +0800

    sort: Add insersion sort vanilla binary

--- sort/src/bin/insertion_sort_vanilla.rs
+++ sort/src/bin/insertion_sort_vanilla.rs
@@ -2,14 +2,14 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
-use sort::insertion_sort;
+use sort::insertion_sort_vanilla;
 use sort::util::{is_sorted, read_ints, show_brief};
 
 fn main() {
     let mut list = read_ints();
-    println!("[InsertionSort] LIST:");
+    println!("[InsertionSort Vanilla] LIST:");
     show_brief(&list);
-    insertion_sort(&mut list);
+    insertion_sort_vanilla(&mut list);
     println!("RESULT:");
     assert!(is_sorted(&list));
     show_brief(&list);
