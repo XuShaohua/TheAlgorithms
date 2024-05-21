5f32a0140db5e8ab641c19f9e73294c2355324a1
Xu Shaohua <shaohua@biofan.org>
2024-05-21 10:38:13 +0800

    leetcode: Add method 2 to 0169

--- src/leetcode/0169.majority-element/src/main.rs
+++ src/leetcode/0169.majority-element/src/main.rs
@@ -11,6 +11,39 @@ pub fn majority_element1(nums: Vec<i32>) -> i32 {
     nums[nums.len() / 2]
 }
 
+// 摩尔投票法
+pub fn majority_element2(nums: Vec<i32>) -> i32 {
+    let mut major: i32 = 0;
+    let mut count: usize = 0;
+    for &num in &nums {
+        if count == 0 {
+            // 如果 count 为 0, 把当前数作为 major
+            major = num;
+        } else if num == major {
+            // 如果当前整数与 major 相等, 计数就加 1
+            count += 1;
+        } else {
+            // 如果当前整数与 major 不相等, 计数减 1
+            count -= 1;
+        }
+    }
+
+    // 检验是否真的超过一半
+    let mut check_count: usize = 0;
+    for &num in &nums {
+        if num == major {
+            check_count += 1;
+        }
+    }
+
+    if check_count * 2 > nums.len() {
+        major
+    } else {
+        // 如果没有超过一半, 就返回 -1
+        -1
+    }
+}
+
 pub type SolutionFn = fn(Vec<i32>) -> i32;
 
 fn check_solution(func: SolutionFn) {
@@ -23,14 +56,20 @@ fn check_solution(func: SolutionFn) {
 
 fn main() {
     check_solution(majority_element1);
+    check_solution(majority_element2);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{check_solution, majority_element1};
+    use super::{check_solution, majority_element1, majority_element2};
 
     #[test]
     fn test_majority_element1() {
         check_solution(majority_element1);
+    }
+
+    #[test]
+    fn test_majority_element2() {
+        check_solution(majority_element2);
     }
 }
