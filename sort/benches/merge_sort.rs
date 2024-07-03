ce98a57780d07b6393e77bf508fabc7c10b9eeb2
Xu Shaohua <shaohua@biofan.org>
2024-07-03 15:44:50 +0800

    sort: Add benchmark for merge sort

---
+++ sort/benches/merge_sort.rs
@@ -0,0 +1,37 @@
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use sort::merge_sort::merge_sort;
+use sort::util::random_ints;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    for exp in 1..5 {
+        let len: usize = 2 * 10_usize.pow(exp);
+        let arr = random_ints(len).expect("Failed to generate random integers");
+        let title1 = format!("std_sort_for_merge_sort {len}");
+        let title2 = format!("merge_sort {len}");
+        let mut arr_sorted = arr.clone();
+        arr_sorted.sort();
+
+        c.bench_function(&title1, |b| {
+            b.iter(|| {
+                let mut arr1 = arr.clone();
+                arr1.sort();
+                assert_eq!(arr1, arr_sorted);
+            })
+        });
+        c.bench_function(&title2, |b| {
+            b.iter(|| {
+                let mut arr2 = arr.clone();
+                merge_sort(&mut arr2);
+                assert_eq!(arr2, arr_sorted);
+            })
+        });
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
