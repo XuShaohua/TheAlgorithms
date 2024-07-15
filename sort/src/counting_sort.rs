d4a7e09e76c226cbaecb74549dfc5efb43790ff2
Xu Shaohua <shaohua@biofan.org>
2024-07-15 18:25:00 +0800

    sort: Fix error in counting-sort-generic

--- sort/src/counting_sort.rs
+++ sort/src/counting_sort.rs
@@ -6,44 +6,45 @@ use std::ops::Sub;
 
 pub fn counting_sort_generic<T>(arr: &mut [T])
 where
-    T: Copy + Default + Ord + Sub<Output=T> + Into<usize>,
+    T: Copy + Default + Ord + Sub<Output=T> + TryInto<usize>,
 {
     if arr.is_empty() {
         return;
     }
     let min_num: T = arr.iter().min().copied().unwrap_or_default();
     let max_num: T = arr.iter().max().copied().unwrap_or_default();
+    // 计算数值范围
     let range: T = max_num - min_num;
-    let size: usize = range.into();
+    let size: usize = range.try_into().unwrap_or_default() + 1;
+
     // 构造计数数组
-    let mut counts = vec![0_usize; size];
+    let mut count_arr = vec![0_usize; size];
 
     // 遍历数组, 更新计数数组
     for num in arr.iter() {
         let delta: T = *num - min_num;
-        let index: usize = delta.into();
-        counts[index] += 1;
+        let index: usize = delta.try_into().unwrap_or_default();
+        count_arr[index] += 1;
     }
 
-    // 生成累积数组
+    // 生成累积数组, prefix sum array
     for i in 1..size {
-        counts[i] += counts[i - 1];
+        count_arr[i] += count_arr[i - 1];
     }
 
-    // 反向填充目标数组
-    let len = arr.len();
-    let mut mirror: Vec<T> = Vec::new();
-    mirror.extend_from_slice(arr);
+    // 构造输入数组, 只读的
+    let input_arr = arr.to_vec();
 
-    for i in (0..len).rev() {
-        let num: T = mirror[i];
-        let diff: T = num - min_num;
-        let index: usize = counts[diff.into()];
+    for &num in input_arr.iter().rev() {
+        let delta: T = num - min_num;
+        let delta_index: usize = delta.try_into().unwrap_or_default();
+        // 从 count_arr 里取出该数值的相对位置
+        let num_index: usize = count_arr[delta_index];
         // 把 num 放在对应的位置
-        arr[index - 1] = num;
+        arr[num_index - 1] = num;
 
-        // 同时更新 counts
-        counts[index] -= 1;
+        // 同时更新 count_arr, 使之计数减1, 这样的话下一个相同数值的元素的索引值就被左移了一位.
+        count_arr[delta_index] -= 1;
     }
 }
 
@@ -54,7 +55,7 @@ pub fn counting_sort(arr: &mut [i32]) {
     }
     let min_num: i32 = arr.iter().min().copied().unwrap_or_default();
     let max_num: i32 = arr.iter().max().copied().unwrap_or_default();
-    // 计算数值范围.
+    // 计算数值范围
     let range: i32 = max_num - min_num;
     let size: usize = range as usize + 1;
 
@@ -93,7 +94,7 @@ pub fn counting_sort(arr: &mut [i32]) {
 
 #[cfg(test)]
 mod tests {
-    use super::counting_sort;
+    use super::{counting_sort, counting_sort_generic};
 
     #[test]
     fn test_counting_sort() {
@@ -110,6 +111,30 @@ mod tests {
             -987_905, -980_069, -977_640,
         ];
         counting_sort(&mut list);
+        assert_eq!(
+            list,
+            [
+                -998_166, -996_360, -995_703, -995_238, -995_066, -994_740, -992_987, -987_905,
+                -983_833, -980_069, -977_640,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_counting_sort_generic() {
+        let mut list = [0, 5, 3, 2, 2];
+        counting_sort_generic(&mut list);
+        assert_eq!(list, [0, 2, 2, 3, 5]);
+
+        let mut list = [-2, -5, -45];
+        counting_sort_generic(&mut list);
+        assert_eq!(list, [-45, -5, -2]);
+
+        let mut list = [
+            -998_166, -996_360, -995_703, -995_238, -995_066, -994_740, -992_987, -983_833,
+            -987_905, -980_069, -977_640,
+        ];
+        counting_sort_generic(&mut list);
         assert_eq!(
             list,
             [
