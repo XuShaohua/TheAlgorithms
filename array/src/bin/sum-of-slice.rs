d7fc5948cfb7f58c980ed366256a5d4954d9f96b
Xu Shaohua <shaohua@biofan.org>
2024-07-27 22:08:46 +0800

    array: Add sum-of-slice

--- array/src/bin/sum-of-slice.rs
+++ array/src/bin/sum-of-slice.rs
@@ -13,7 +13,17 @@ fn prefix_sum(numbers: &[i32]) -> Vec<i32> {
 }
 
 fn main() {
-    let arr = [10, 4, 16, 20];
+    let arr = [8, 19, 28, 21, 33, 97, 62, 7, 45];
     let ps = prefix_sum(&arr);
-    println!("prefix sum array: {ps:?}");
+    for left in 0..2 {
+        for right in 3..arr.len() {
+            let sum = if left == 0 {
+                ps[right]
+            } else {
+                ps[right] - ps[left - 1]
+            };
+            let sum_slice = arr[left..=right].iter().sum();
+            assert_eq!(sum, sum_slice);
+        }
+    }
 }
