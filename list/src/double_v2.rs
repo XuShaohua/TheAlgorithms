f4952520ff45c768c84786ebb22f8cd994877bd6
Xu Shaohua <shaohua@biofan.org>
2024-07-28 22:51:21 +0800

    list: Support push/pop elements

--- list/src/double_v2.rs
+++ list/src/double_v2.rs
@@ -105,19 +105,29 @@ impl<T> LinkedList<T> {
 
     pub fn insert(&mut self) {}
 
-    pub fn push_front() {}
+    /// Add an element to the beginning of list.
+    pub fn push_front(&mut self, value: T) {
+        let node = Box::new(Node::new(value));
+        let node_ptr = NonNull::from(Box::leak(node));
+        self.push_back_node(node_ptr);
+    }
 
+    /// Add an element to the end of list.
     pub fn push_back(&mut self, value: T) {
         let node = Box::new(Node::new(value));
         let node_ptr = NonNull::from(Box::leak(node));
-        unsafe {
-            self.push_back_node(node_ptr);
-        }
+        self.push_back_node(node_ptr);
     }
 
-    pub fn pop_front() {}
+    /// Remove the first element in the list.
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.pop_front_node().map(Node::into_inner)
+    }
 
-    pub fn pop_back() {}
+    /// Remove the last element in the list.
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.pop_back_node().map(Node::into_inner)
+    }
 
     /// Swap the contents.
     pub fn swap(&mut self) {}
@@ -171,13 +181,31 @@ impl<T> LinkedList<T> {
 
 // Private or unsafe functions for list.
 impl<T> LinkedList<T> {
-    unsafe fn push_back_node(&mut self, node_ptr: NonNull<Node<T>>) {
-        (*node_ptr.as_ptr()).next = None;
-        (*node_ptr.as_ptr()).prev = self.tail;
+    fn push_front_node(&mut self, node_ptr: NonNull<Node<T>>) {
+        unsafe {
+            (*node_ptr.as_ptr()).next = self.head;
+            (*node_ptr.as_ptr()).prev = None;
+        }
+        let node = Some(node_ptr);
+
+        match self.head {
+            Some(head) => unsafe { (*head.as_ptr()).prev = node },
+            None => self.tail = node,
+        }
+
+        self.head = node;
+        self.len += 1;
+    }
+
+    fn push_back_node(&mut self, node_ptr: NonNull<Node<T>>) {
+        unsafe {
+            (*node_ptr.as_ptr()).next = None;
+            (*node_ptr.as_ptr()).prev = self.tail;
+        }
         let node = Some(node_ptr);
 
         match self.tail {
-            Some(tail) => (*tail.as_ptr()).next = node,
+            Some(tail) => unsafe { (*tail.as_ptr()).next = node },
             None => self.head = node,
         }
 
@@ -185,7 +213,35 @@ impl<T> LinkedList<T> {
         self.len += 1;
     }
 
-    //unsafe fn push_front_node(&mut self, node_ptr: NonNull<Node
+    fn pop_front_node(&mut self) -> Option<Box<Node<T>>> {
+        self.head.map(|old_head| {
+            let old_head = unsafe { Box::from_raw(old_head.as_ptr()) };
+            self.head = old_head.next;
+
+            match self.head {
+                Some(head) => unsafe { (*head.as_ptr()).prev = None },
+                None => self.tail = None,
+            }
+
+            self.len -= 1;
+            old_head
+        })
+    }
+
+    fn pop_back_node(&mut self) -> Option<Box<Node<T>>> {
+        self.tail.map(|old_tail| {
+            let old_tail = unsafe { Box::from_raw(old_tail.as_ptr()) };
+            self.tail = old_tail.prev;
+
+            match self.tail {
+                Some(tail) => unsafe { (*tail.as_ptr()).next = None },
+                None => self.head = None,
+            }
+
+            self.len -= 1;
+            old_tail
+        })
+    }
 }
 
 impl<T> Node<T> {
@@ -201,7 +257,7 @@ impl<T> Node<T> {
 
     #[must_use]
     #[inline]
-    fn into_value(self) -> T {
+    fn into_inner(self: Box<Self>) -> T {
         self.value
     }
 }
